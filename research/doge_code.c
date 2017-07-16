2f24d397ca9bbe3acbbce66f68ad7590b91dcdaa
Matthew D. Huff <matthewhuff89@gmail.com>
2017-07-16 19:11:43 -0400

    Added example adjective and noun parsers

---
+++ research/doge_code.c
@@ -0,0 +1,18 @@
+#include "mpc.h"
+
+int main(int argc, char** argv) {
+    /* Build a parser 'Adjective to recognize descriptions */
+    mpc_parser_t* Adjective = mpc_or(4,
+      mpc_sym("wow"), mpc_sym("many"),
+      mpc_sym("so"), mpc_sym("such")
+    );
+
+    mpc_parser_t* Noun = mpc_or(5, 
+      mpc_sym("lisp"), mpc_sym("language"), 
+      mpc_sym("book"), mpc_sym("build"),
+      mpc_sym("c")
+    );
+    return 0;
+}
+
+
