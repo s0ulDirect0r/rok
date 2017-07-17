03f5fdfe894cef89ca42b9dcbaef1e199ce83d11
Matthew D. Huff <matthewhuff89@gmail.com>
2017-07-17 10:37:42 -0400

    Added more 'doge' code and grammar rules

---
+++ research/doge_grammar.c
@@ -0,0 +1,13 @@
+#include "mpc.h"
+
+int main(int argc, char** argc) {
+    mpc_parser_t* Adjective = mpc_new("adjective");
+    mpc_parser_t* Noun = mpc_new("noun");
+    mpc_parser_t* Phrase = mpc_new
+
+    mpca_lang(MPCA_LANG_DEFAULT, 
+      "                                \
+        adjective : \"wow\" | \"many\" \
+                  | \"so\"
+    
+}
