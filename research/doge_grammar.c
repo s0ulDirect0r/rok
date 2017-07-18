7ba8f9d0739fe0db25869870783d0cd43949d13e
Matthew D. Huff <matthewhuff89@gmail.com>
2017-07-18 13:40:58 -0400

    Finished adding second doge_grammar script

--- research/doge_grammar.c
+++ research/doge_grammar.c
@@ -5,9 +5,17 @@ int main(int argc, char** argc) {
     mpc_parser_t* Noun = mpc_new("noun");
     mpc_parser_t* Phrase = mpc_new
 
-    mpca_lang(MPCA_LANG_DEFAULT, 
-      "                                \
-        adjective : \"wow\" | \"many\" \
-                  | \"so\"
-    
+    mpca_lang(MPCA_LANG_DEFAULT,
+      "                                           \
+        adjective : \"wow\" | \"many\"            \
+                  | \"so\"  | \"such\";           \
+        noun      : \"lisp\" | \"language\"       \
+                  | \"book\" | \"build\" | \"c\"; \
+        phrase    : <adjective> <noun>;           \
+        doge      : <phrase>*;                    \
+      ",
+      Adjective, Noun, Phrase, Doge);
+
+    /* Do some parsing... */
+    mpc_cleanup(4, ADjective, Noun, Phrase, Doge);
 }
