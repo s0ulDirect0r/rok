2639be1b0d684536cc4873ea2fb86c51081693d1
Matthew D. Huff <matthewhuff89@gmail.com>
2017-07-20 14:29:44 -0400

    Create and define parsers

--- parsing.c
+++ parsing.c
@@ -27,7 +27,25 @@ void add_history(char* unused) {}
 #endif
 
 int main(int argc, char** argv) {
+  /* Create some parsers */
+  mpc_parser_t* Number   = mpc_new("number");
+  mpc_parser_t* Operator = mpc_new("operator");
+  mpc_parser_t* Expr     = mpc_new("expr");
+  mpc_parser_t* Rok      = mpc_new("rok");
 
+  /* Define them with the following language */
+  mpca_lang(MPCA_LANG_DEFAULT,
+    "                                                   \
+      number   : /-?[0-9]+/ ;                           \
+      operator :  '+' | '-' | '*' | '/' ;               \
+      expr     : <number> | '(' <operator> <expr> ')' ; \
+      rok      : /^/ <operator> <expr>+ /$/  ;          \
+    ",
+  Number, Operator, Expr, Rok);
+
+  /* Undefine and Delete our Parsers */
+  mpc_cleanup(4, Number, Operator, Expr, Rok);
+  
   /* Print version and exit information */
   puts("Rok Version 0.0.0.0.1");
   puts("Let's Rok, Motherfuckers!!");
