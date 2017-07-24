5720256b29c47b1b2d8a201aa8c619ab0ade2735
Matthew D. Huff <matthewhuff89@gmail.com>
2017-07-23 23:43:53 -0400

    Added files for regex research

---
+++ research/mureg.c
@@ -0,0 +1,10 @@
+/* How do i export this into other areas of the language? */
+
+/* Write a regular expression matching strings of all a or b such as aababa or bbaa. */
+#include <regex.h>
+#include "mureg.h"
+
+int mureg(char*) {
+
+  return 0
+}
---
+++ research/mureg.h
@@ -0,0 +1,10 @@
+// Mureg Regex Library (for shits and giggles)
+#ifndef mureg_h
+#define mureg_h
+
+/* Matches regex */
+/* This is a function declaration */
+/* We declare it here so we can use it else where */
+int mureg(char*);
+
+#endif
