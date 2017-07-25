5f01ab8692340d67ceb9dae54eeba249c3e80170
Matthew D. Huff <matthewhuff89@gmail.com>
2017-07-25 15:39:29 -0400

    Add eval function

--- parsing.c
+++ parsing.c
@@ -74,3 +74,25 @@ int main(int argc, char** argv) {
   mpc_cleanup(4, Number, Operator, Expr, Rok);
   return 0;
 }
+
+long eval(mpc_ast_t* t) {
+  /* if tagged as number return it directly. */
+  if (strstr(t->tag, "number")) {
+    return atoi(t->contents);
+  }
+
+  /* the operator is always second child */
+  char* op = t->children[1]->contents;
+
+  /* We store the third child in x */
+  long x = eval(t->children[2]);
+
+  /* Iterate the remaining children and combining */
+  int i = 3;
+  while(strstr(t->children[i]->tag, "expr")) {
+    x = eval_op(x, op, eval(t->children[i]));
+    i++;
+  }
+
+  return x;
+}
