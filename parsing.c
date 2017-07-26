e9e4643e2dbc79867f7fde4a5e00950b3f6c5110
Matthew D. Huff <matthewhuff89@gmail.com>
2017-07-26 08:18:25 -0400

    Add evaluation function for operators

--- parsing.c
+++ parsing.c
@@ -26,6 +26,38 @@ void add_history(char* unused) {}
 #include <editline/readline.h>
 #endif
 
+/* Use operator string to see which operation to perform */
+long eval_op(long value1, char* op, long value2) {
+  if(strcmp(op, "+") == 0) { return value1 + value2; }
+  if(strcmp(op, "-") == 0) { return value1 - value2; }
+  if(strcmp(op, "*") == 0) { return value1 * value2; }
+  if(strcmp(op, "/") == 0) { return value1 / value2; }
+  if(strcmp(op, "%") == 0) { return value1 % value2; }
+  return 0;
+}
+
+long eval(mpc_ast_t* t) {
+  /* if tagged as number return it directly. */
+  if (strstr(t->tag, "number")) {
+    return atoi(t->contents);
+  }
+
+  /* the operator is always second child */
+  char* op = t->children[1]->contents;
+
+  /* We store the third child in x */
+  long x = eval(t->children[2]);
+
+  /* Iterate the remaining children and combining */
+  int i = 3;
+  while(strstr(t->children[i]->tag, "expr")) {
+    x = eval_op(x, op, eval(t->children[i]));
+    i++;
+  }
+
+  return x;
+}
+
 int main(int argc, char** argv) {
   /* Create some parsers */
   mpc_parser_t* Number   = mpc_new("number");
@@ -58,7 +90,8 @@ int main(int argc, char** argv) {
     mpc_result_t r;
     if (mpc_parse("<stdin>", input, Rok, &r)) {
       /* On Success Print the AST */
-      mpc_ast_print(r.output);
+      long result = eval(r.output);
+      printf("%li\n", result);
       mpc_ast_delete(r.output);
     } else {
       /* Otherwise print the error */
@@ -73,26 +106,4 @@ int main(int argc, char** argv) {
   /* Undefine and Delete our Parsers */
   mpc_cleanup(4, Number, Operator, Expr, Rok);
   return 0;
-}
-
-long eval(mpc_ast_t* t) {
-  /* if tagged as number return it directly. */
-  if (strstr(t->tag, "number")) {
-    return atoi(t->contents);
-  }
-
-  /* the operator is always second child */
-  char* op = t->children[1]->contents;
-
-  /* We store the third child in x */
-  long x = eval(t->children[2]);
-
-  /* Iterate the remaining children and combining */
-  int i = 3;
-  while(strstr(t->children[i]->tag, "expr")) {
-    x = eval_op(x, op, eval(t->children[i]));
-    i++;
-  }
-
-  return x;
 }
