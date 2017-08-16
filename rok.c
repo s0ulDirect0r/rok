d53e59e7f930ecf63920efb8fbbc6b7136e82199
Matthew D. Huff <matthewhuff89@gmail.com>
2017-08-15 21:11:08 -0400

    Finish sexpression evaluation function

--- rok.c
+++ rok.c
@@ -165,13 +165,28 @@ lval* lval_eval_sexpr(lval* v) {
     v->cell[i] = lval_eval(v->cell[i]);
   }
 
-  /* TO DO:
-    ADD ERROR CHECKING
-    EMPTY EXPRESSIONS
-    SINGLE EXPRESSIONS
-    ENSURE SYMBOL type
-    CALL BUILTIN WITH OPERATOR
-    */
+  /* Error checking */
+  for (int i = 0; i < v->count; i++) {
+    if (v->cell[i]->type == LVAL_ERR) { return lval_take(v, i); }
+  }
+
+  /* Empty Expression */
+  if (v->count == 0) { return v; }
+
+  /* Single Expression */
+  if (v->count == 1) { return lval_take(v, 0); }
+
+  /* Ensure First Element is Symbol */
+  lval* f = lval_pop(v, 0);
+  if (f->type != LVAL_SYM) {
+    lval_del(f); lval_del(v);
+    return lval_err("S-expression Does not start with symbol!");
+  }
+
+  /* Call built in operator */
+  lval* result = builtin_op(v, f->sym);
+  lval_del(f);
+  return result;
 }
 
 /* Print an "lval" followed by a newline */
