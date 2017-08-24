19b0f5103a538ba4bf6edce352e6de2c2cfb5faa
Matthew D. Huff <matthewhuff89@gmail.com>
2017-08-24 08:59:56 -0400

    Add builtin symbols to symbol parser

--- rok.c
+++ rok.c
@@ -290,7 +290,8 @@ int main(int argc, char** argv) {
   /* Define them with the following Language */
   mpca_lang(MPCA_LANG_DEFAULT,
     " number   : /[+-]?([0-9]*[.])?[0-9]+/ ;               "
-    " symbol   : '+' | '-' | '*' | '/' | '%' | '^' ;       "
+    " symbol   : \"list\" | \"head\" | \"tail\" | \"join\" |\
+     \"eval\" | '+' | '-' | '*' | '/' | '%' | '^' ;        "
     " sexpr    : '(' <expr>* ')' ;                         "
     " qexpr    : '{' <expr>* '}' ;                         "
     " expr     : <number> | <symbol> | <sexpr> | <qexpr> ; "
