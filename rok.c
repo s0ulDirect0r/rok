50f81a59d6dbb5057e76940c109ee8fc11aed9bf
Matthew D. Huff <matthewhuff89@gmail.com>
2017-08-25 16:32:43 -0400

    Add builtin_head function

--- rok.c
+++ rok.c
@@ -237,6 +237,31 @@ lval* builtin_op(lval* a, char* op) {
   lval_del(a); return x;
 }
 
+lval* builtin_head(lval* a) {
+  /* Check Error Conditions */
+  if (a->count != 1) {
+    lval_del(a);
+    return lval_err("Function 'head' passed too many arguments!")
+  }
+
+  if (a->cell[0]->type != LVAL_QEXPR) {
+    lval_del(a);
+    return lval_err("Function 'head' passed incorrect types!");
+  }
+
+  if (a->cell[0]->count == 0) {
+    lval_del(a);
+    return lval_err("Function 'head' passed {}!");
+  }
+
+  /* Otherwise take first argument */
+  lval* v = lval_take(a, 0);
+
+  /* Delete all elements that are not head and return */
+  while(v->count > 1) { lval_del(lval_pop(v, 1)); }
+  return v;
+}
+
 lval* lval_eval_sexpr(lval* v) {
 
   /* Evaluate Children */
