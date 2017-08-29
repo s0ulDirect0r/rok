81d7e7c2891682639365f5ea5b154b77a81858bc
Matthew D. Huff <matthewhuff89@gmail.com>
2017-08-29 13:37:35 -0400

    Refactor head and tail to use LASSERT macro

--- rok.c
+++ rok.c
@@ -242,20 +242,12 @@ lval* builtin_op(lval* a, char* op) {
 
 lval* builtin_head(lval* a) {
   /* Check Error Conditions */
-  if (a->count != 1) {
-    lval_del(a);
-    return lval_err("Function 'head' passed too many arguments!");
-  }
-
-  if (a->cell[0]->type != LVAL_QEXPR) {
-    lval_del(a);
-    return lval_err("Function 'head' passed incorrect types!");
-  }
-
-  if (a->cell[0]->count == 0) {
-    lval_del(a);
-    return lval_err("Function 'head' passed {}!");
-  }
+  LASSERT(a, a->count == 1,
+     "Function 'head' passed too many arguments!");
+  LASSERT(a, a->cell[0]->type != LVAL_QEXPR,
+     "Function 'head' passed incorrect types!");
+  LASSERT(a, a->cell[0]->count == 0,
+     "Function 'head' passed {}!");
 
   /* Otherwise take first argument */
   lval* v = lval_take(a, 0);
@@ -267,20 +259,12 @@ lval* builtin_head(lval* a) {
 
 lval* builtin_tail(lval* a) {
   /* Check Error Conditions */
-  if (a->count != 1) {
-    lval_del(a);
-    return lval_err("Function 'tail' passed too many arguments!");
-  }
-
-  if (a->cell[0]->type != LVAL_QEXPR) {
-    lval_del(a);
-    return lval_err("Function 'tail' passed incorrect types!");
-  }
-
-  if (a->cell[0]->count == 0) {
-    lval_del(a);
-    return lval_err("Function 'tail' passed {}!");
-  }
+  LASSERT(a, a->count != 1, 
+    "Function 'tail' passed too many arguments!");
+  LASSERT(a, a->cell[0]->type != LVAL_QEXPR,
+    "Function 'tail' passed incorrect types!");
+  LASSERT(a, a->cell[0]->count == 0,
+    "Function 'tail' passed {}!");
 
   /* Other wise take first arg */
   lval* v = lval_take(a, 0);
