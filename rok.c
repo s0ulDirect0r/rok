1b7656524849a4d44ebdda8fd8bc4f98237350e7
Matthew D. Huff <matthewhuff89@gmail.com>
2017-09-05 13:30:09 -0400

    Reposition function declarations to fix compilation

--- rok.c
+++ rok.c
@@ -268,80 +268,6 @@ lval* lval_copy(lval* v) {
   return x;
 }
 
-/** Lenv functions **/
-lenv* lenv_new(void) {
-  lenv* e = malloc(sizeof(lenv));
-  e->count = 0;
-  e->syms = NULL;
-  e->vals = NULL;
-  return e;
-}
-
-void lenv_del(lenv* e) {
-  for(int i = 0; i < e->count; i++) {
-    free(e->syms[i]);
-    lval_del(e->vals[i]);
-  }
-  free(e->syms);
-  free(e->vals);
-  free(e);
-}
-
-lval* lenv_get(lenv* e, lval* k) {
-  /* Iterate over all items in the environment */
-  for (int i = 0; i < e->count; i++) {
-    if(strcmp(e->syms[i], k->sym) == 0) {
-      return lval_copy(e->vals[i]);
-    }
-  }
-  return lval_err("unbound symbol!");
-}
-
-void lenv_put(lenv* e, lval* k, lval* v) {
-  /* Iterate */
-  for (int i = 0; i < e->count; i++) {
-    if(strcmp(e->syms[i], k->sym) == 0) {
-      lval_del(e->vals[i]);
-      e->vals[i] = lval_copy(v);
-      return;
-    }
-  }
-
-  /* If no existing entry found, allocate space for new entry */
-  e->count++;
-  e->vals = realloc(e->vals, sizeof(lval*) * e->count);
-  e->syms = realloc(e->syms, sizeof(char*) * e->count);
-
-  /* Copy contents of lval and symbol string to new location */
-  e->vals[e->count-1] = lval_copy(v);
-  e->syms[e->count-1] = malloc(strlen(k->sym)+1);
-  strcpy(e->syms[e->count-1], k->sym);
-}
-
-void lenv_add_builtin(lenv* e, char* name, lbuiltin func) {
-  lval* k = lval_sym(name);
-  lval* v = lval_fun(func);
-  lenv_put(e, k, v);
-  lval_del(k); lval_del(v);
-}
-
-void lenv_add_builtins(lenv* e) {
-  /* List functions */
-  lenv_add_builtin(e, "list", builtin_list);
-  lenv_add_builtin(e, "head", builtin_head);
-  lenv_add_builtin(e, "tail", builtin_tail);
-  lenv_add_builtin(e, "eval", builtin_eval);
-  lenv_add_builtin(e, "len", builtin_len);
-  lenv_add_builtin(e, "join", builtin_join);
-  
-  /* Math functions */
-  lenv_add_builtin(e, "+", builtin_add);
-  lenv_add_builtin(e, "-", builtin_sub);
-  lenv_add_builtin(e, "*", builtin_mul);
-  lenv_add_builtin(e, "/", builtin_div);
-}
-
-
 lval* builtin_op(lenv* e, lval* a, char* op) {
   /* Ensure all arguments are numbers */
   for (int i = 0; i < a->count; i++) {
@@ -514,6 +440,37 @@ lval* lval_eval_sexpr(lenv* e, lval* v) {
   return result;
 }
 
+lval* lenv_get(lenv* e, lval* k) {
+  /* Iterate over all items in the environment */
+  for (int i = 0; i < e->count; i++) {
+    if(strcmp(e->syms[i], k->sym) == 0) {
+      return lval_copy(e->vals[i]);
+    }
+  }
+  return lval_err("unbound symbol!");
+}
+
+void lenv_put(lenv* e, lval* k, lval* v) {
+  /* Iterate */
+  for (int i = 0; i < e->count; i++) {
+    if(strcmp(e->syms[i], k->sym) == 0) {
+      lval_del(e->vals[i]);
+      e->vals[i] = lval_copy(v);
+      return;
+    }
+  }
+
+  /* If no existing entry found, allocate space for new entry */
+  e->count++;
+  e->vals = realloc(e->vals, sizeof(lval*) * e->count);
+  e->syms = realloc(e->syms, sizeof(char*) * e->count);
+
+  /* Copy contents of lval and symbol string to new location */
+  e->vals[e->count-1] = lval_copy(v);
+  e->syms[e->count-1] = malloc(strlen(k->sym)+1);
+  strcpy(e->syms[e->count-1], k->sym);
+}
+
 lval* lval_eval(lenv* e, lval* v) {
   if (v->type == LVAL_SYM) {
     lval* x = lenv_get(e, v);
@@ -528,6 +485,48 @@ lval* lval_eval(lenv* e, lval* v) {
 
 /* Print an "lval" followed by a newline */
 void lval_println(lval* v) { lval_print(v); putchar('\n'); }
+
+/** Lenv functions **/
+lenv* lenv_new(void) {
+  lenv* e = malloc(sizeof(lenv));
+  e->count = 0;
+  e->syms = NULL;
+  e->vals = NULL;
+  return e;
+}
+
+void lenv_del(lenv* e) {
+  for(int i = 0; i < e->count; i++) {
+    free(e->syms[i]);
+    lval_del(e->vals[i]);
+  }
+  free(e->syms);
+  free(e->vals);
+  free(e);
+}
+
+void lenv_add_builtin(lenv* e, char* name, lbuiltin func) {
+  lval* k = lval_sym(name);
+  lval* v = lval_fun(func);
+  lenv_put(e, k, v);
+  lval_del(k); lval_del(v);
+}
+
+void lenv_add_builtins(lenv* e) {
+  /* List functions */
+  lenv_add_builtin(e, "list", builtin_list);
+  lenv_add_builtin(e, "head", builtin_head);
+  lenv_add_builtin(e, "tail", builtin_tail);
+  lenv_add_builtin(e, "eval", builtin_eval);
+  lenv_add_builtin(e, "len", builtin_len);
+  lenv_add_builtin(e, "join", builtin_join);
+
+  /* Math functions */
+  lenv_add_builtin(e, "+", builtin_add);
+  lenv_add_builtin(e, "-", builtin_sub);
+  lenv_add_builtin(e, "*", builtin_mul);
+  lenv_add_builtin(e, "/", builtin_div);
+}
 
 int main(int argc, char** argv) {
   /* Create some parsers */
