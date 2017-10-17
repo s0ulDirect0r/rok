8dad5cd87e867efdf0c40b37d4502b5103f3557a
Matthew D. Huff <matthewhuff89@gmail.com>
2017-10-17 08:50:16 -0400

    Using var instead of sym for arg name

--- lenv.c
+++ lenv.c
@@ -22,25 +22,25 @@ void lenv_del(lenv* env) {
   free(env);
 }
 
-lval* lenv_get(lenv* env, lval* sym) {
+lval* lenv_get(lenv* env, lval* var) {
   /* Iterate over all items in the environment */
   for (int i = 0; i < env->count; i++) {
-    if(strcmp(env->syms[i], sym->sym) == 0) {
+    if(strcmp(env->syms[i], var->sym) == 0) {
       return lval_copy(env->vals[i]);
     }
   }
 
   if (env->parent) {
-    return lenv_get(env->parent, sym);
+    return lenv_get(env->parent, var);
   } else {
-    return lval_err("unbound symbol '%s'!", sym->sym);
+    return lval_err("unbound symbol '%s'!", var->sym);
   }
 }
 
-void lenv_put(lenv* env, lval* sym, lval* val) {
+void lenv_put(lenv* env, lval* var, lval* val) {
   /* Iterate */
   for (int i = 0; i < env->count; i++) {
-    if(strcmp(env->syms[i], sym->sym) == 0) {
+    if(strcmp(env->syms[i], var->sym) == 0) {
       lval_del(env->vals[i]);
       env->vals[i] = lval_copy(val);
       return;
@@ -54,8 +54,8 @@ void lenv_put(lenv* env, lval* sym, lval* val) {
 
   /* Copy contents of lval and symbol string to new location */
   env->vals[env->count-1] = lval_copy(val);
-  env->syms[env->count-1] = malloc(strlen(sym->sym)+1);
-  strcpy(env->syms[env->count-1], sym->sym);
+  env->syms[env->count-1] = malloc(strlen(var->sym)+1);
+  strcpy(env->syms[env->count-1], var->sym);
 }
 
 lenv* lenv_copy(lenv* env) {
@@ -72,18 +72,18 @@ lenv* lenv_copy(lenv* env) {
   return copy;
 }
 
-void lenv_def(lenv* env, lval* sym, lval* val) {
+void lenv_def(lenv* env, lval* var, lval* val) {
   /* Iterate till env has no parent */
   while (env->parent) { env = env->parent; }
   /* Put value in */
-  lenv_put(env, sym, val);
+  lenv_put(env, var, val);
 }
 
 void lenv_add_builtin(lenv* env, char* name, lbuiltin func) {
-  lval* sym = lval_sym(name);
+  lval* var = lval_sym(name);
   lval* val = lval_fun(func);
-  lenv_put(env, sym, val);
-  lval_del(sym); lval_del(val);
+  lenv_put(env, var, val);
+  lval_del(var); lval_del(val);
 }
 
 void lenv_add_builtins(lenv* env) {
--- lenv.h
+++ lenv.h
@@ -16,10 +16,10 @@ struct lenv {
 
 lenv* lenv_new(void);
 void lenv_del(lenv* env);
-struct lval* lenv_get(lenv* env, struct lval* sym);
-void lenv_put(lenv* env, struct lval* sym, struct lval* val);
+struct lval* lenv_get(lenv* env, struct lval* var);
+void lenv_put(lenv* env, struct lval* var, struct lval* val);
 lenv* lenv_copy(lenv* env);
-void lenv_def(lenv* env, struct lval* sym, struct lval* val);
+void lenv_def(lenv* env, struct lval* var, struct lval* val);
 void lenv_add_builtin(lenv* env, char* name, lbuiltin func);
 void lenv_add_builtins(lenv* env);
 
