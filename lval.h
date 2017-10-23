6d761c737d54dfe5bfe46bb413be1091739a44d4
Matthew D. Huff <matthewhuff89@gmail.com>
2017-10-23 08:07:37 -0400

    Refactor lval header arg names

--- lval.h
+++ lval.h
@@ -32,26 +32,26 @@ struct lval {
 /* Declare Enumerations for lval types */
 enum lval_types { LVAL_NUM, LVAL_ERR, LVAL_SYM, LVAL_SEXPR, LVAL_QEXPR, LVAL_FUN };
 
-lval* lval_num(long x);
+lval* lval_num(long num);
 lval* lval_err(char* fmt, ...);
 lval* lval_sym(char* sym);
 lval* lval_sexpr(void);
 lval* lval_qexpr(void);
 lval* lval_fun(lbuiltin fun);
-void lval_del(lval* v);
-lval* lval_eval_sexpr(struct lenv* e, lval* v);
-lval* lval_eval(struct lenv* e, lval* v);
-void lval_println(lval* v);
-lval* lval_read_num(mpc_ast_t* t);
-lval* lval_read(mpc_ast_t* t);
-void lval_expr_print(lval* v, char open, char close);
-void lval_print(lval* v);
-lval* lval_pop(lval* v, int i);
-lval* lval_take(lval* v, int i);
+void lval_del(lval* val);
+lval* lval_eval_sexpr(struct lenv* env, lval* val);
+lval* lval_eval(struct lenv* env, lval* val);
+void lval_println(lval* val);
+lval* lval_read_num(mpc_ast_t* tree);
+lval* lval_read(mpc_ast_t* tree);
+void lval_expr_print(lval* val, char open, char close);
+void lval_print(lval* val);
+lval* lval_pop(lval* val, int index);
+lval* lval_take(lval* val, int index);
 lval* lval_join(lval* x, lval* y);
-lval* lval_copy(lval* v);
+lval* lval_copy(lval* val);
 lval* lval_lambda(lval* formals, lval* body);
-lval* lval_call(struct lenv* e, lval* f, lval* a);
-char* ltype_name(int t);
+lval* lval_call(struct lenv* env, lval* func, lval* args);
+char* ltype_name(int type);
 
 #endif
